f807d5876cf32bb3be8375a483b8ff8fa00ca261
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-16 12:56:06 -0700

    Added user model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+require('./models/User');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://shreesh:tracks2k20!@cluster0.onczm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo', err);
+})
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
